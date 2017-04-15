e801fcc3039eb26b26d9d2b5ecac2475055f44c6
Aleksandar Markovic <aleksandar23markovic@gmail.com>
2017-04-15 23:10:58 +0200

    Working on slider

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    slides = $('#crossfade');
+    slides.find('li').hide().filter(':first').show();
+    setInterval(slideshow, 3000);
+});
+
+var slides;
+
+function slideshow() {
+    slides.find('li:first').fadeOut('slow').next().fadeIn('slow').end().appendTo('#crossfade');
+} 
