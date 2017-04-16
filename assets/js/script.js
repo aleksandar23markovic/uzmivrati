5914e3bd9a5204da50eccbb62154f97e48a38c40
Aleksandar Markovic <aleksandar23markovic@gmail.com>
2017-04-16 14:54:40 +0200

    Repair flexslider

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
     $('.flexslider').flexslider({
     		animation: "slide"
   	});
+
+  	$('.flex-next').on('click',function () {
+    $('.flexslider').flexslider("play");
+	});
+	$('.flex-prev').on('click',function () {
+	    $('.flexslider').flexslider("play");
+	});
   
 });
 
