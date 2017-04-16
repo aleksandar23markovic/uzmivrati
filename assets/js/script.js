a30c8f75bc23e7cabafb1062779afd3142815dfc
Aleksandar Markovic <aleksandar23markovic@gmail.com>
2017-04-16 10:26:43 +0200

    Implement flexslider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-    slides = $('#crossfade');
+    /* slides = $('#crossfade');
     slides.find('li').hide().filter(':first').show();
-    setInterval(slideshow, 3000);
+    setInterval(slideshow, 3000); */
+    $('.flexslider').flexslider({
+    		animation: "slide"
+  	});
+  
 });
 
-var slides;
+/* var slides;
 
 function slideshow() {
     slides.find('li:first').fadeOut('slow').next().fadeIn('slow').end().appendTo('#crossfade');
-} 
+} */
