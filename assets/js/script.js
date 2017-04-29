8bc0aba99513c136d557a83563380d29dbcbdf31
Aleksandar Markovic <aleksandar23markovic@gmail.com>
2017-04-29 20:00:17 +0200

    Few fixes and started OPTIONS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    /* slides = $('#crossfade');
-    slides.find('li').hide().filter(':first').show();
-    setInterval(slideshow, 3000); */
     $('.flexslider').flexslider({
     		animation: "slide"
   	});
@@ -11,12 +8,5 @@ $(document).ready(function() {
 	});
 	$('.flex-prev').on('click',function () {
 	    $('.flexslider').flexslider("play");
-	});
-  
+	}); 
 });
-
-/* var slides;
-
-function slideshow() {
-    slides.find('li:first').fadeOut('slow').next().fadeIn('slow').end().appendTo('#crossfade');
-} */
